19efbe8342251b6a69c65c4076a91302fe17cc86
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-09 00:58:36 +0530

    fix : settings project

--- Chats/routing.py
+++ Chats/routing.py
@@ -2,11 +2,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chatapp.routing
 
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chatapp.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chatapp.routing.websocket_urlpatterns)
+        ),
+    }
+)
