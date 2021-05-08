0003834b2eb1b6e59ec3ba7c62a21ea3950235b7
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-09 01:32:46 +0530

    fix : Message register removed

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-
-from .models import Message
-
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['auuthor', 'content', 'timestamp']
-
-
-# Register your models here.
-
-admin.site.register(Message)
