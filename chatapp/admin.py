a65450d956b11139b0b612339f5c197ecf9fec20
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-10 11:31:51 +0530

    fix : Chats bugg solved

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["auuthor", "content", "timestamp"]
+
+
+# Register your models here.
+
+admin.site.register(Message)
--- chatapp/routing.py
+++ chatapp/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 
-from .consumers import ChatConsumer
+from chatapp.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer),
 ]
