ddfe1449e1dda4b6a9532ed7c3f60b2db3c2c469
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-06-07 15:19:57 +0530

    feat : add create method MessageView

--- message/models.py
+++ message/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 class Message(models.Model):
 
-    sender = models.ForeignKey("user_control.CustomUser", on_delete=models.CASCADE)
-    reciever = models.ForeignKey("user_control.CustomUser", on_delete=models.CASCADE)
+    sender = models.ForeignKey(
+        "user_control.CustomUser",
+        related_name="message_sender",
+        on_delete=models.CASCADE,
+    )
+    reciever = models.ForeignKey(
+        "user_control.CustomUser",
+        related_name="message_reciever",
+        on_delete=models.CASCADE,
+    )
     message = models.TextField(blank=True, null=True)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,7 +28,9 @@ class Message(models.Model):
 
 class MessageAttachment(models.Model):
 
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    message = models.ForeignKey(
+        Message, related_name="message_attachments", on_delete=models.CASCADE
+    )
     attachment = models.FileField(upload_to="message/attachments")
     caption = models.CharField(max_length=225, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- message/views.py
+++ message/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import Message, MessageAttachment, MessageSerializer
 
 # Create your views here.
+
+
+class MessageView(ModelViewSet):
+    queryset = Message.objects.select_related("sender", "reciever").prefetch_related(
+        "message_attachments"
+    )
+    serializer_class = MessageSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+
+        attachments = request.data.pop("attachments", None)
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        if attachments:
+            MessageAttachment.objects.bulk_create(
+                [
+                    MessageAttachment(**attachment, message_id=serializer.data["id"])
+                    for attachment in attachments
+                ]
+            )
+
+            message_data = self.get_queryset().get(id=serializer.data["id"])
+
+            return Response(self.serializer_class(message_data).data, status=201)
+
+        return Response(serializer.data, status=201)
