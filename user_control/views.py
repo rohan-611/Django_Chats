1921b297dd5326cd436080eb0122d0f1dcae3510
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-06-14 10:14:22 +0530

    feat : UserProfile Search Implemented

--- user_control/views.py
+++ user_control/views.py
@@ -1,24 +1,24 @@
-import jwt
 import random
+import re
 import string
 from datetime import datetime, timedelta
 
+import jwt
 from django.conf import settings
 from django.contrib.auth import authenticate
-
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 
-from .models import JWT, CustomUser
 from .authentication import Authentication
+from .models import JWT, CustomUser
 from .serializers import (
     LoginSerializer,
-    RegisterSerializer,
     RefreshSerializer,
-    UserProfileSerializer,
+    RegisterSerializer,
     UserProfile,
+    UserProfileSerializer,
 )
 
 
@@ -119,3 +119,44 @@ class UserProfileView(ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        data = self.request.query_params.dict()
+        keyword = data.get("keyword", None)
+
+        if keyword:
+            search_fields = ("user__username", "first_name", "last_name", "user__email")
+            query = self.get_query(keyword, search_fields)
+            return self.queryset.filter(query).distinct()
+
+        return self.queryset
+
+    @staticmethod
+    def get_query(query_string, search_fields):
+
+        query = None
+        terms = UserProfileView.normalize_query(query_string)
+
+        for term in terms:
+            or_query = None
+            for field_name in search_fields:
+                q = Q(**{"%s_icontains" % field_name: term})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query = or_query | q
+
+            if query is None:
+                query = or_query
+            else:
+                query = or_query
+
+        return query
+
+    @staticmethod
+    def normalize_query(
+        query_string,
+        findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+        normspace=re.compile(r"\s{2,}").sub,
+    ):
+        return [normspace(" ", (t[0] or t[1]).strip()) for t in findterms(query_string)]
